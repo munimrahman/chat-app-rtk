c19bff9f1d6731ba2098df7bc721eab7ccc38b06
munimrahman <munimrh@gmail.com>
2023-03-23 01:43:26 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Conversation from "./pages/Conversation";
+import Inbox from "./pages/Inbox";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import useAuthChecked from "./hooks/useAuthCheck";
+import PublicRoute from "./components/PublicRoute";
+import PrivateRoute from "./components/PrivateRoute";
+
+function App() {
+  const authChecked = useAuthChecked();
+
+  return !authChecked ? (
+    <div>
+      <h1>Checking Authentication.....</h1>
+    </div>
+  ) : (
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <PublicRoute>
+              <Login />
+            </PublicRoute>
+          }
+        />
+        <Route
+          path="/register"
+          element={
+            <PublicRoute>
+              <Register />
+            </PublicRoute>
+          }
+        />
+        <Route
+          path="/inbox"
+          element={
+            <PrivateRoute>
+              <Conversation />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/inbox/:id"
+          element={
+            <PrivateRoute>
+              <Inbox />
+            </PrivateRoute>
+          }
+        />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/app/store.js
@@ -0,0 +1,17 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { apiSlice } from "../features/api/apiSlice";
+import authSliceReducer from "../features/auth/authSlice";
+import conversationsSliceReducer from "../features/conversations/conversationSlice";
+import messagesSliceReducer from "../features/messages/messageSlice";
+
+export const store = configureStore({
+  reducer: {
+    [apiSlice.reducerPath]: apiSlice.reducer,
+    auth: authSliceReducer,
+    conversations: conversationsSliceReducer,
+    messages: messagesSliceReducer,
+  },
+  devTools: process.env.NODE_ENV !== "production",
+  middleware: (getDefaultMiddlewares) =>
+    getDefaultMiddlewares().concat(apiSlice.middleware),
+});
