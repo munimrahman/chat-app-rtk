b3ce381c3e0ed73625685ab02545541afef8199e
munimrahman <munimrh@gmail.com>
2023-03-24 09:16:46 +0600

    message

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -4,7 +4,6 @@ import useAuth from "../hooks/useAuth";
 
 const PrivateRoute = ({ children }) => {
   const isLoggedIn = useAuth();
-  console.log(isLoggedIn);
   return isLoggedIn ? children : <Navigate to={"/"} />;
 };
 
